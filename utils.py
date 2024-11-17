bd1ac6178ac8ca903a79b93062caf8b7c47db359
linhui7 <lin.hui@nyu.edu>
2024-11-17 18:00:18 -0500

    patch generation successful

--- utils.py
+++ utils.py
@@ -11,42 +11,71 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from torch.utils.data.sampler import SubsetRandomSampler
 
+
+# Set a fixed seed for reproducibility
+torch.manual_seed(42)
+np.random.seed(42)
+
+
 # Load the datasets
 # We randomly sample some images from the dataset, because ImageNet itself is too large.
-def dataloader(train_size, test_size, data_dir, batch_size, num_workers, total_num=50000):
+def dataloader(
+    train_size, test_size, data_dir, batch_size, num_workers, total_num=50000
+):
     # Setup the transformation
-    train_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(224),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
+    train_transforms = transforms.Compose(
+        [
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
 
-    test_transforms = transforms.Compose([
-        transforms.Resize(size=(224, 224)),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
-    ])
+    test_transforms = transforms.Compose(
+        [
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261)),
+        ]
+    )
 
-    index = np.arange(total_num)
-    np.random.shuffle(index)
-    train_index = index[:train_size]
-    test_index = index[train_size: (train_size + test_size)]
+    train_dataset = torchvision.datasets.CIFAR10(
+        root="./data", train=True, transform=train_transforms, download=True
+    )
+    test_dataset = torchvision.datasets.CIFAR10(
+        root="./data", train=False, transform=test_transforms, download=True
+    )
 
-    train_dataset = torchvision.datasets.ImageFolder(root=data_dir, transform=train_transforms)
-    test_dataset = torchvision.datasets.ImageFolder(root=data_dir, transform=test_transforms)
+    train_indices = np.arange(len(train_dataset))
+    np.random.shuffle(train_indices)
+    train_indices = train_indices[:train_size]
 
-    train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(train_index), num_workers=num_workers, pin_memory=True, shuffle=False)
-    test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, sampler=SubsetRandomSampler(test_index), num_workers=num_workers, pin_memory=True, shuffle=False)
+    test_indices = np.arange(len(test_dataset))
+    np.random.shuffle(test_indices)
+    test_indices = test_indices[:test_size]
+
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(train_indices),
+        num_workers=num_workers,
+    )
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=batch_size,
+        sampler=SubsetRandomSampler(test_indices),
+        num_workers=num_workers,
+    )
+
     return train_loader, test_loader
+
 
 # Test the model on clean dataset
 def test(model, dataloader):
     model.eval()
     correct, total, loss = 0, 0, 0
     with torch.no_grad():
-        for (images, labels) in dataloader:
+        for images, labels in dataloader:
             images = images.cuda()
             labels = labels.cuda()
             outputs = model(images)
@@ -55,11 +84,12 @@ def test(model, dataloader):
             correct += (predicted == labels).sum().item()
     return correct / total
 
+
 # Load the log and generate the training line
 def log_generation(log_dir):
     # Load the statistics in the log
     epochs, train_rate, test_rate = [], [], []
-    with open(log_dir, 'r') as f:
+    with open(log_dir, "r") as f:
         reader = csv.reader(f)
         flag = 0
         for i in reader:
@@ -73,8 +103,8 @@ def log_generation(log_dir):
 
     # Generate the success line
     plt.figure(num=0)
-    plt.plot(epochs, test_rate, label='test_success_rate', linewidth=2, color='r')
-    plt.plot(epochs, train_rate, label='train_success_rate', linewidth=2, color='b')
+    plt.plot(epochs, test_rate, label="test_success_rate", linewidth=2, color="r")
+    plt.plot(epochs, train_rate, label="train_success_rate", linewidth=2, color="b")
     plt.xlabel("epoch")
     plt.ylabel("success rate")
     plt.xlim(-1, max(epochs) + 1)
